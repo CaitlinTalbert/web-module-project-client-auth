50e4569fae2d54e778a7b6f7bceab244fd053cfe
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-02-22 14:52:29 -0600

    login working and re routes to friends list

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,63 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
+  const { push } = useHistory();
+  const [cred, setCred] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setCred({
+      ...cred,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // console.log(cred);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:9000/api/login", cred)
+      .then((resp) => {
+        localStorage.setItem("token", resp.payload);
+        push("/friends");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
       <h2>Login</h2>
       <form>
         <div>
           <label htmlFor="username">Username:</label>
-          <input id="username" placeholder="Username" />
+          <input
+            onChange={handleChange}
+            name="username"
+            id="username"
+            placeholder="Username"
+          />
         </div>
         <br />
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" placeholder="Password" />
+          <input
+            onChange={handleChange}
+            name="password"
+            type="password"
+            id="password"
+            placeholder="Password"
+          />
         </div>
         <br />
-        <button>Submit</button>
+        <button onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   );
