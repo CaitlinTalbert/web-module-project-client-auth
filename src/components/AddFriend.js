d0eb214bd385e23f0185e1780bfebfa95edc1bd6
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-02-22 14:36:48 -0600

    basic DOM written for each component

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 const AddFriend = () => {
-  return <h2>Add Friend</h2>;
+  return (
+    <div>
+      <h2>Add Friend</h2>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input id="username" placeholder="Username" />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="age">Age:</label>
+          <input id="age" placeholder="Age" />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input id="email" placeholder="Email" />
+        </div>
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddFriend;
